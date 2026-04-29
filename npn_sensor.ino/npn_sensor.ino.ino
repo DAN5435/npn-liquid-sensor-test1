b004efed8fe455a7205f6961c2e94f143a80e06b
DAN5345 <zaffrydanish@gmail.com>
2026-04-29 15:22:29 +0800

    index.html

--- npn_sensor.ino/npn_sensor.ino.ino
+++ npn_sensor.ino/npn_sensor.ino.ino
@@ -1,26 +1,55 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "Funny2";
+const char* password = "mamyissues";
 
-const int sensorPin = 2; 
+
+
+String apiKey = "KI8EGPCHY1H0N2B0";
+
+const int sensorPin = D4;  // GPIO2
+
+const char* server = "api.thingspeak.com";
 
 void setup() {
-  Serial.begin(9600);
-  // INPUT_PULLUP is essential for NPN (sinking) sensors
-  pinMode(sensorPin, INPUT_PULLUP); 
-  Serial.println("NPN Sensor Ready");
+  Serial.begin(115200);
+  pinMode(sensorPin, INPUT_PULLUP);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
 }
 
 void loop() {
   int val = digitalRead(sensorPin);
 
-  // For NPN, LOW typically means "Detected" 
+  int fieldValue;
   if (val == LOW) {
-    Serial.println("Liquid Detected!");
+    fieldValue = 1;   // Have IV
+    Serial.println("Have IV");
   } else {
-    Serial.println("--- Empty ---");
+    fieldValue = 0;   // Low IV
+    Serial.println("Low IV");
   }
-  
-  delay(500);
+
+  WiFiClient client;
+
+  if (client.connect(server, 80)) {
+    String url = "/update?api_key=" + apiKey + "&field1=" + String(fieldValue);
+
+    Serial.println(url);  // DEBUG
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: api.thingspeak.com\r\n" +
+                 "Connection: close\r\n\r\n");
+  }
+
+  delay(15000);  // VERY IMPORTANT (ThingSpeak limit)
 }
